a9b990ccc39f46adddd69672e1e2a65c5aaf0abb
Welliton gervickas <wellitogervickas@gmail.com>
2021-02-20 18:58:46 -0300

    renamed fns

--- src/validators/length.ts
+++ src/validators/length.ts
@@ -1,11 +1,19 @@
-import { T, always, compose, __, cond, gte, curry } from 'ramda';
+import {
+  T as Else,
+  gte as greaterThanOrEqual,
+  compose as If,
+  always as Returns,
+  __ as bypassArg,
+  cond,
+  curry
+} from 'ramda';
 
 const getLength = curry((password: String): Number => password.length);
 
 const lengthValidator = cond([
-  [compose(gte(__, 17), getLength), always('strong')],
-  [compose(gte(__, 13), getLength), always('medium')],
-  [T, () => 'weak']
+  [If(greaterThanOrEqual(bypassArg, 17), getLength), Returns('strong')],
+  [If(greaterThanOrEqual(bypassArg, 13), getLength), Returns('medium')],
+  [Else, () => 'weak']
 ]);
 
 export default lengthValidator;
