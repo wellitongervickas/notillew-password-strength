41832653e210c123d198e20a6deaf47e17a016cd
Welliton gervickas <wellitogervickas@gmail.com>
2021-02-20 22:21:45 -0300

    creating sequence matcher

--- src/validators/sequences.test.ts
+++ src/validators/sequences.test.ts
@@ -1,6 +1,18 @@
 import sequencesValidator from './sequences';
 
 describe('Sequences Validator', () => {
-  it('should be weak', () => expect(sequencesValidator()).toBe(true));
-  it('should be strong', () => expect(sequencesValidator()).toBe(true));
+  it('should be weak', () => {
+    expect(sequencesValidator('fedcba')).toBe(0);
+    expect(sequencesValidator('abcdef')).toBe(0);
+    expect(sequencesValidator('123456')).toBe(0);
+    expect(sequencesValidator('654321')).toBe(0);
+    expect(sequencesValidator('gfd654cba321')).toBe(0);
+    expect(sequencesValidator('000000000000000000')).toBe(0);
+  });
+
+  it('should be strong', () => {
+    expect(sequencesValidator('lsnujn17hsihda')).toBe(2);
+    expect(sequencesValidator('925d122d1962a2112')).toBe(2);
+    expect(sequencesValidator('78123yhd7@&DH@+15')).toBe(2);
+  });
 });
--- src/validators/sequences.ts
+++ src/validators/sequences.ts
@@ -1,3 +1,42 @@
-const sequencesValidator = () => true;
+import { T as Else, always as returns, cond, curry, difference } from 'lodash/fp';
+
+const decimalNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+const isSequencialCharacters = curry((password: string): boolean => {
+  return false;
+});
+
+const sequencesValidator = cond<string, number>([
+  [isSequencialCharacters, returns(0)],
+  [Else, () => 2]
+]);
 
 export default sequencesValidator;
